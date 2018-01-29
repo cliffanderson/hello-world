7d5f180dc8c2935c0e5b77bc4ad40948c6204a8b
Cliff Anderson <canderson@onecloudlabs.com>
2018-01-29 09:29:47 -0500

    Add hello to Bob

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 		System.out.println("Here ye : important announcement");
 
 		System.out.println("Hello, World!");
+		System.out.println("Hello, Bob!");
 
 		System.out.println("Farewell friends");
 	}
